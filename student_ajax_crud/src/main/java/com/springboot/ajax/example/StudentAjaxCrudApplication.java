bca1a5ddca07ae876b3474134b7a7af04df5f07a
Khushali Bhanushali <khushibhanushali2008@gmail.com>
2023-02-07 12:20:59 +0530

    initial commit

---
+++ student_ajax_crud/src/main/java/com/springboot/ajax/example/StudentAjaxCrudApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.ajax.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class StudentAjaxCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAjaxCrudApplication.class, args);
+	}
+
+}
